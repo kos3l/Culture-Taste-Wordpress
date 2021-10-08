3d9bfad1a0510df77f3fb4bbd27a29fedbe6cd6f
kos3l <71730597+kos3l@users.noreply.github.com>
2021-10-08 15:52:39 +0200

    adding order to css and adding all the components to the home page

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <?php if(have_posts()):?>
                 <?php while(have_posts()): the_post() ?>
-                    <div class="row landing-page m-0" >
+                    <div class="row landing-page m-0" id="home" >
                             <img src="<?php the_field('home_background') ?>" class="bg-img p-0" alt="">
                             <div class="col center p-0">
                                 <div class="pop-up-box">
@@ -13,11 +13,19 @@
                                 </div>
                             </div>
                     </div>
-                    <div class="row landing-page m-0" >
+                    <div class="row filters-title m-0" id="recipes">
                         <h1>RECIPES</h1>
                     </div>
                     <div class="row filters m-0" >
                         <div class="col"></div>
+                    </div>
+                    <div class="row recipe m-0" >
+                        <div class="col"></div>
+                        <div class="col"></div>
+                        <div class="col"></div>
+                    </div>
+                    <div class="row" id="books">
+
                     </div>
                 <?php endwhile; ?>
             <?php endif; ?>
